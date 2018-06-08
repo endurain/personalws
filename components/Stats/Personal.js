07da53559c36ed1ee88ac78abc71129623b58aa2
Zac Sanders <37851511+endurain@users.noreply.github.com>
2018-06-08 00:43:42 -0600

    Add files via upload

---
+++ components/Stats/Personal.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Table from './Table';
+import data from '../../data/stats';
+
+class PersonalStats extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
+    const birthTime = new Date('1984-03-05T09:24:00');
+    this.setState({
+      data: Object.assign({}, this.state.data, {
+        age: {
+          label: 'Current age',
+          value: ((Date.now() - birthTime) / divisor).toFixed(9),
+        },
+      }),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Some stats about me</h3>
+        <Table
+          data={Object.keys(this.state.data).map(key => this.state.data[key])}
+        />
+      </div>
+    );
+  }
+}
+
+export default PersonalStats;
---
+++ data/contact.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    link: 'https://github.com/mldangelo',
+    label: 'Github',
+    icon: 'icon-github-circled',
+  },
+  {
+    link: 'https://facebook.com/d',
+    label: 'Facebook',
+    icon: 'icon-facebook',
+  },
+  {
+    link: 'https://www.instagram.com/dangelosaurus/',
+    label: 'Instagram',
+    icon: 'icon-instagram',
+  },
+  {
+    link: 'https://www.linkedin.com/in/michaelldangelo',
+    label: 'LinkedIn',
+    icon: 'icon-linkedin',
+  },
+  {
+    link: 'https://angel.co/michael-d-angelo',
+    label: 'Angel List',
+    icon: 'icon-angellist',
+  },
+  {
+    link: 'https://twitter.com/dangelosaurus',
+    label: 'Twitter',
+    icon: 'icon-twitter',
+  },
+  {
+    link: 'mailto:michael.l.dangelo@gmail.com',
+    label: 'Email',
+    icon: 'icon-mail-alt',
+  },
+];
+
+export default data;
